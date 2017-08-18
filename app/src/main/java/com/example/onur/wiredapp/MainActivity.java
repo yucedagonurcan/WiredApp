de325efb4763eb1a7b281287ab53666d376d5460
Onur Yücedağ <olmaditekrar@gmail.com>
2017-08-18 22:29:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/onur/wiredapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.onur.wiredapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    String[] Names = {"Onur","Mihri","Serdar","Okan","Berkay","Gamze","Oluculer"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+    }
+}
